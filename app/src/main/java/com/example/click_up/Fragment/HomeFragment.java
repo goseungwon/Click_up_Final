17a746427b1ac0dd6204599133012607eb44ab60
goseungwon <knmy0925@gmail.com>
2022-06-05 16:23:37 +0900

    distance 함수 추가

--- app/src/main/java/com/example/click_up/Fragment/HomeFragment.java
+++ app/src/main/java/com/example/click_up/Fragment/HomeFragment.java
@@ -86,6 +86,7 @@ public class HomeFragment extends Fragment {
     public void onStart() {
         super.onStart();
         get_friend();
+        Log.d("거리 M단위", String.valueOf(distance(37.560959, 127.193786, 37.564769, 127.192161))+ "M");
     }
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
@@ -292,6 +293,41 @@ public class HomeFragment extends Fragment {
                 dialog.dismiss();
             }
         });
+    }
+
+
+    /**
+     * 거리 계산 메서드
+     *
+     * @param lat1 지점 1 위도
+     * @param lon1 지점 1 경도
+     * @param lat2 지점 2 위도
+     * @param lon2 지점 2 경도
+     * @return
+     */
+    private static double distance(double lat1, double lon1, double lat2, double lon2) {
+
+        double theta = lon1 - lon2;
+        double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
+
+        dist = Math.acos(dist);
+        dist = rad2deg(dist);
+        dist = dist * 60 * 1.1515 * 1609.344;
+
+
+
+        return (dist);
+    }
+
+
+    // This function converts decimal degrees to radians
+    private static double deg2rad(double deg) {
+        return (deg * Math.PI / 180.0);
+    }
+
+    // This function converts radians to decimal degrees
+    private static double rad2deg(double rad) {
+        return (rad * 180 / Math.PI);
     }
 
     private void marker_Post() {
