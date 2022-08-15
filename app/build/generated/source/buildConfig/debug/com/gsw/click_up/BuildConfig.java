a7322cfedb3f25116ff1abdcdddf3da9270ea43a
goseungwon <knmy0925@gmail.com>
2022-08-15 13:25:49 +0900

    package 이름 변경(googleservice.json)

--- app/build/generated/source/buildConfig/debug/com/gsw/click_up/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/gsw/click_up/BuildConfig.java
@@ -5,7 +5,7 @@ package com.gsw.click_up;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.example.click_up_final";
+  public static final String APPLICATION_ID = "com.gsw.click_up";
   public static final String BUILD_TYPE = "debug";
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "1.0";
